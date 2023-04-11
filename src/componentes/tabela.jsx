48b1160a0d5f6b840be21dcda14def7e3061b375
caio-faustino <caioexfaustino.pro@gmail.com>
2023-04-10 23:56:58 -0300

    codigo refatorado sem testes

--- src/componentes/tabela.jsx
+++ src/componentes/tabela.jsx
@@ -140,3 +140,92 @@ export default function Table() {
           
         />
       </label>
+
+      <button
+      type="button"
+      onClick={ lidarClique }
+        data-testid="button-filter"
+        
+      >
+        {' '}
+        FILTRAR
+        {' '}
+      </button>
+
+      <button
+      type="button"
+      onClick={ deletaTudo }
+        data-testid="button-remove-filters"
+        
+      >
+        {' '}
+        Remover todas filtragens
+        {' '}
+      </button>
+
+      {
+        ArrayDeColunas.map((index) => (
+          <p
+            key={ index.colunaAtual }
+            data-testid="filter"
+          >
+            {index.colunaAtual}
+            {index.comparacaoAtual}
+            {index.numeroMagico}
+
+            <button
+              type="button"
+              onClick={ () => deletaApenasUm(index.colunaAtual) }
+            >
+              Remover
+            </button>
+          </p>
+        ))
+      }
+
+      <hr />
+
+
+      {/* REQUISITO 01 */}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>Url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todosFiltrados.map((elemento, index) => (
+            <tr key={ index }>
+              <td data-testid="elemento">{elemento.name}</td>
+              <td>{elemento.rotation_period}</td>
+              <td>{elemento.orbital_period}</td>
+              <td>{elemento.diameter}</td>
+              <td>{elemento.climate}</td>
+              <td>{elemento.gravity}</td>
+              <td>{elemento.terrain}</td>
+              <td>{elemento.surface_water}</td>
+              <td>{elemento.population}</td>
+              <td>{elemento.films}</td>
+              <td>{elemento.created}</td>
+              <td>{elemento.edited}</td>
+              <td>{elemento.url}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    </>
+
+  );
+}
